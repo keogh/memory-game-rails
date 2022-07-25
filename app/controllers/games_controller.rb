350c39741591afc6869cbde5e77a8e683e281484
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2022-07-25 16:59:34 -0600

    Initial game views

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,33 @@
+class GamesController < ApplicationController
+  def index
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    # create players
+    # create cards (2x num_pairs)
+
+    if @game.save
+      redirect_to games_path, notice: "Game was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:num_players, :num_pairs)
+  end
+end
